76f3293a6bb86cbccd50a5c2efdde92a868a3a1a
AsaPlaysMC <fafafa609@gmail.com>
2017-07-27 19:48:16 +0300

    deleted:    .eslintrc.json 	modified:   app.js 	modified:   lib/fileS3.js 	modified:   routes/shorten/polr.js

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const Router = require('./lib/superhero.js');
 
 // Check for required environment variables
 for (const env of [
-  'DATABASE_URL',
-  'S3_FILES_ACCESS_KEY_ID',
-  'S3_FILES_SECRET_KEY',
-  'S3_FILES_BUCKET',
-  // 'S3_FILES_ENDPOINT_URL', optional
-  'S3_LINKS_ACCESS_KEY_ID',
-  'S3_LINKS_SECRET_KEY',
-  'S3_LINKS_BUCKET',
+  'DATABASE_URL', // Postgres DB URL (postgres://user:password@localhost/database)
+  'S3_FILES_ACCESS_KEY_ID', // S3 API Access Key ID
+  'S3_FILES_SECRET_KEY', // S3 API Secret Key
+  'S3_FILES_BUCKET', // S3 API Bucket
+      // Optional settings (Ignore if you won't use them)
+  //'S3_FILES_ENDPOINT_URL', optional
+  //'S3_LINKS_ACCESS_KEY_ID',
+  //'S3_LINKS_SECRET_KEY',
+  //'S3_LINKS_BUCKET',
   'PORT'
 ]) {
   if (!process.env.hasOwnProperty(env)) {
@@ -22,7 +23,7 @@ for (const env of [
 // Create a router and apply routes
 var router = new Router();
 router.use(authorize);
-router.get('/shorten/polr', require('./routes/shorten/polr.js')); // GET /shorten/polr
+//router.get('/shorten/polr', require('./routes/shorten/polr.js')); // GET /shorten/polr
 router.post('/upload/pomf', require('./routes/upload/pomf.js')); // POST /upload/pomf
 router.post('/users', require('./routes/users/create.js'));
 router.get('/health', (req, res) => res.end(204, ''));
